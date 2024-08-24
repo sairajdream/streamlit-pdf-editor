ff7952fda45c96fe85ebfd1af24459518d515860
aBDULQADARmANASAWALA <aqmanasawala@gmail.com>
2024-08-24 18:31:49 +0530

    fixing bus 004

--- streamlit_pdf_editor.py
+++ streamlit_pdf_editor.py
@@ -2,13 +2,12 @@ import streamlit as st
 import pymupdf
 
 st.title("Personal PDF Editor")
-st.image("./Picture1.png")
 uploaded_files = st.file_uploader("Choose a PDF file", type="pdf")
 
 if uploaded_files:
+    doc = pymupdf.open(uploaded_files)
     st.write(uploaded_files.name.removesuffix(".pdf"))
     name = uploaded_files.name.removesuffix(".pdf")
-    doc = pymupdf.open(uploaded_files)
     for pg_no in range(len(doc)):
         doc[pg_no].insert_image(pymupdf.Rect(353, 87, 560, 106), filename = "./Picture1.png")
     doc.save(f"{name}_e.pdf")
