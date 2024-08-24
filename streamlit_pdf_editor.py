6c1b55db56897620cbcbce401c210bd646be7d6e
Abdulqadar Manasawala <72400908+aBDULQADARmANASAWALA@users.noreply.github.com>
2024-08-24 17:15:16 +0530

    adding pdf editor python file

---
+++ streamlit_pdf_editor.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pymupdf
+
+st.title("Personal PDF Editor")
+uploaded_files = st.file_uploader("Choose a PDF file", type="pdf")
+
+if uploaded_files:
+    name = uploaded_files.name.removesuffix(".pdf")
+    doc = pymupdf.open(uploaded_files)
+    for pg_no in range(len(doc)):
+        doc[pg_no].insert_image(pymupdf.Rect(353, 87, 560, 106), filename = "./Picture1.png")
+    doc.save(f"{name}_e.pdf")
+    doc.close()
+    with open(f"{name}_e.pdf", "rb") as f:
+        pdf_byte = f.read()
+        st.download_button("Download PDF", pdf_byte, file_name=f"{name}_e.pdf", mime="application/pdf")
