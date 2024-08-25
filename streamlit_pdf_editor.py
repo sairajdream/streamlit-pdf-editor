008d7d26c18b0f801110faa7a91fefe48ba0eea1
aBDULQADARmANASAWALA <aqmanasawala@gmail.com>
2024-08-25 19:08:54 +0530

    trying out a logic with new library

--- streamlit_pdf_editor.py
+++ streamlit_pdf_editor.py
@@ -4,6 +4,18 @@ from pypdf import PdfReader, PdfWriter
 st.title("Personal PDF Editor")
 uploadedfile = st.file_uploader("Choose a PDF file")
 
+if uploadedfile is not None:
+    name = uploadedfile.name.removesuffix(".pdf")
+
+    reader_pg = PdfReader(uploadedfile).pages[0]
+    writer_pg = PdfWriter(clone_from="stamp.pdf")
+
+    writer_pg.pages[0].merge_page(reader_pg, over=False)
+
+    writer_pg.write(f"{name}_e.pdf")
+    with open(f"{name}_e.pdf") as pdf:
+        st.write("open success")
+
 with open("2.pdf", "rb") as pdf:
     byte = pdf.read()
     st.download_button("Download PDF", byte, file_name="2.pdf", mime="application/pdf")
